ccab711580f906aa508490edf85755b1e97e5c5c
Shen9ke <2592528345@qq.com>
2017-11-11 11:27:55 +0800

    update

---
+++ content/Tetris.md
@@ -0,0 +1,9 @@
+# Tetris (俄罗斯方块
+
+* 该俄罗斯方块是用cocos creator 写的; 
+* 语言： javascript
+
+UI样式为我们一前玩的几块钱的小型掌上游戏机。
+	
+[演示地址](http://eveaos.com/demo/index.html)
+   
---
+++ demo/Tetris/src/settings.js
@@ -0,0 +1,155 @@
+_CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "BaseCollider",
+        "Boundary"
+    ],
+    collisionMatrix: [
+        [
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            false
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "d3kQVtKEZHe5feFLNB1+ID": [
+                "picture/GameTipsCanvas.png",
+                0
+            ],
+            "cfbHeowDFNX7swhObkuZTZ": [
+                "picture/background.png",
+                0
+            ],
+            "33GBbGenVLbJJqgoUnRoRT": [
+                "picture/background1.png",
+                0
+            ],
+            "0chGxZyY9HZICgaEXUiSM+": [
+                "picture/base.png",
+                0
+            ],
+            "8eonDtWS1O8p0BkAXt00hh": [
+                "picture/button/bt_cha_dark.png",
+                0
+            ],
+            "fdPLMmwK9GQr/So4c9NNlu": [
+                "picture/button/bt_cha_light.png",
+                0
+            ],
+            "96izrIA5hOkZkVjBiI3bT5": [
+                "picture/button/bt_down_dark.png",
+                0
+            ],
+            "1dUr5kGSZDnL36lEDN2Q+R": [
+                "picture/button/bt_down_light.png",
+                0
+            ],
+            "aeWV3/kX5ChaujdRwzNBeM": [
+                "picture/button/bt_left_dark.png",
+                0
+            ],
+            "f8tbtP89ZD4L/ptfSX2+7x": [
+                "picture/button/bt_left_light.png",
+                0
+            ],
+            "810cVykoNFv7HsnD2HHndS": [
+                "picture/button/bt_menu_dark.png",
+                0
+            ],
+            "e9h/sMCmhIj7mfBDAh7577": [
+                "picture/button/bt_menu_light.png",
+                0
+            ],
+            "c4D9VwOG1HC6fXdH+91LBs": [
+                "picture/button/bt_right_dark.png",
+                0
+            ],
+            "8e8rSGFLdDJ5x1OlQwLVnM": [
+                "picture/button/bt_right_light.png",
+                0
+            ],
+            "e148eDMolMdoan2cmlCYmm": [
+                "picture/button/bt_up_dark.png",
+                0
+            ],
+            "0c9rnmXgRMALCiGhaIdQvw": [
+                "picture/button/bt_up_light.png",
+                0
+            ],
+            "f8DTnUqXZKcYGNEReGmGOw": [
+                "picture/canvas.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/game/main_game.fire",
+    scenes: [
+        {
+            url: "db://assets/game/main_game.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "0274f9480": [
+            "26LKDvyhdDCIbPOcR6q3wr",
+            0,
+            "57RyF0e35B6JSMs/pcZcUM",
+            "5aHS5ckOFB+7YPiVJCqKKS",
+            "63VPDELsVJ14XhZgCpjXst",
+            "63v04OpM1F64duYIEa6FDI",
+            "75JoZFXlZOZL0FtrFRGoY+",
+            "77rRQr0nhIwagoWNvHtQfx",
+            "7ewyh53QtPFo1Clt2uIfQf",
+            "83Jp1ilkhMkKXDIU3ltF+x",
+            "8cHmuQ3hxLJ43r0EXfrJlC",
+            "b6u5SjkVdEH78C3UThIBrj",
+            "c1YJT2SrtCX5R42djHp+/1",
+            "c13pBJcpFAapJP+kaaGE2a",
+            "c5bcUy6cNKjYX5xtcgKO8i",
+            "caBw1iVxtICb509HSkDtw7",
+            "d1qlAkU35EM7jYisBKCbcA",
+            "e1nXNwBctEhLeLrjWapncF",
+            "e3C5Dx9hVLNLL5eKCFT4Ql",
+            "fd6ITm+45G96c+KjMjBV31"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "4eS58XlRRJwJhevQxzPV1j"
+    ],
+    "md5AssetsMap": {
+        "02/0274f9480.json": "d4a3c",
+        "assets/picture/background.png": "54976",
+        "assets/picture/background1.png": "65560",
+        "assets/picture/base.png": "59190",
+        "assets/picture/button/bt_cha_dark.png": "94d1a",
+        "assets/picture/button/bt_cha_light.png": "fe615",
+        "assets/picture/button/bt_down_dark.png": "b9ba4",
+        "assets/picture/button/bt_down_light.png": "36fd5",
+        "assets/picture/button/bt_left_dark.png": "eb772",
+        "assets/picture/button/bt_left_light.png": "8e253",
+        "assets/picture/button/bt_menu_dark.png": "12b51",
+        "assets/picture/button/bt_menu_light.png": "403b6",
+        "assets/picture/button/bt_right_dark.png": "f2211",
+        "assets/picture/button/bt_right_light.png": "692e2",
+        "assets/picture/button/bt_up_dark.png": "1a748",
+        "assets/picture/button/bt_up_light.png": "124eb",
+        "assets/picture/canvas.png": "d4394",
+        "assets/picture/GameTipsCanvas.png": "7b1c8"
+    }
+}
